52c5b9f5b47543e2c741b6ba2cf0af6fb5e80134
YongJieLiu <617805327@qq.com>
2020-03-30 17:37:12 +0800

    连接数据库

--- src/main/java/com/github/Truthcode/Main.java
+++ src/main/java/com/github/Truthcode/Main.java
@@ -12,44 +12,66 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        //未处理的数据池
-        List<String> linkPool = new ArrayList();
-        //已处理的数据池
-        Set<String> processedLinks = new HashSet<>();
-        linkPool.add("https://sina.cn/");
 
-        while (true) {
-            //若连接池为空就退出
-            if (linkPool.isEmpty()) {
-                break;
-            }
-            String link = linkPool.remove(linkPool.size() - 1);
-            //若连接已经被处理即跳过
-            if (processedLinks.contains(link)) {
-                continue;
-            }
-            //若连接是新浪有关的新闻页面就进入
-            if (isInterstingLink(link)) {
-                Document doc = httpGetAndParseHtml(link);
-                Elements links = doc.select("a");
-                for (Element aTag : links) {
-                    linkPool.add(aTag.attr("href"));
-                }
+    private static List<String> loadUrlsFromDatabase(Connection connection, String sql) throws SQLException {
+        List<String> results = new ArrayList<>();
 
-                //假如这个是新闻页面详情页面，就存入数据库
-                storeIntoDatabaseIfItIsNewsPage(doc);
-
-                processedLinks.add(link);
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                results.add(resultSet.getString(1));
             }
         }
+        return results;
     }
+
+    public static void main(String[] args) throws IOException, SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:h2:file:D:/study/workplace/crawler/news");
+
+        //未处理的数据池
+        //从数据库加载即将处理的链接的代码
+        List<String> linkPool = loadUrlsFromDatabase(connection, "select link from LINKS_TO_BE_PROCESSED");
+
+
+        //已处理的数据池
+        //从数据库加载已经处理的链接的代码
+        Set<String> processedLinks = new HashSet<>(loadUrlsFromDatabase(connection, "select link from LINKS_ALREADY_PROCESSED"));
+
+        try {
+            while (true) {
+                //若连接池为空就退出
+                if (linkPool.isEmpty()) {
+                    break;
+                }
+                String link = linkPool.remove(linkPool.size() - 1);
+                //若连接已经被处理即跳过
+                if (processedLinks.contains(link)) {
+                    continue;
+                }
+                //若连接是新浪有关的新闻页面就进入
+                if (isInterstingLink(link)) {
+                    Document doc = httpGetAndParseHtml(link);
+                    Elements links = doc.select("a");
+                    for (Element aTag : links) {
+                        linkPool.add(aTag.attr("href"));
+                    }
+                    //假如这个是新闻页面详情页面，就存入数据库
+                    storeIntoDatabaseIfItIsNewsPage(doc);
+                    processedLinks.add(link);
+                }
+            }
+        }finally {
+            System.out.println("Exit");
+        }
+    }
+
 
     private static void storeIntoDatabaseIfItIsNewsPage(Document doc) {
         Elements articleTags = doc.select("article");
@@ -61,7 +83,6 @@ public class Main {
         }
     }
 
-
     private static Document httpGetAndParseHtml(String link) throws IOException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
 
@@ -72,7 +93,6 @@ public class Main {
 
         HttpGet httpGet = new HttpGet(link);
         httpGet.addHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36)");
-
 
         try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
             System.out.println(response1.getStatusLine());
